cc122272c41e0ad1759490c427da79d473956599
FPruss <fpruss@mailbox.org>
2022-06-10 13:13:09 +0200

    refactoring

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,5 +1,30 @@
 'use strict';
 
+function decodeCredentialCreationOptions(credentialCreationOptions) {
+    const credentialCreationOptionsJson = parseResponse(credentialCreationOptions);
+    logResponse(credentialCreationOptionsJson);
+    credentialCreationOptionsJson.publicKey.challenge = base64ToUint8Array(credentialCreationOptionsJson.publicKey.challenge);
+    credentialCreationOptionsJson.publicKey.user.id = base64ToUint8Array(credentialCreationOptionsJson.publicKey.user.id);
+    credentialCreationOptionsJson.publicKey.excludeCredentials.forEach((credential) => {
+        credential.id = base64ToUint8Array(credential.id)
+    });
+    logNavigatorCreate(credentialCreationOptionsJson);
+    return credentialCreationOptionsJson;
+}
+
+function encodePublicKeyCredential(publicKeyCredential) {
+    return {
+        id: publicKeyCredential.id,
+        rawId: uint8ArrayToBase64(publicKeyCredential.rawId),
+        type: publicKeyCredential.type,
+        response: {
+            attestationObject: uint8ArrayToBase64(publicKeyCredential.response.attestationObject),
+            clientDataJSON: uint8ArrayToBase64(publicKeyCredential.response.clientDataJSON),
+        },
+        clientExtensionResults: publicKeyCredential.getClientExtensionResults()
+    };
+}
+
 function registerUser() {
     const username = $('#username').val();
     if (username === '') {
@@ -28,30 +53,15 @@ function registerUser() {
             datatype: 'json'
         }
     ).then((credentialCreationOptions) => {
-        const credentialCreationOptionsJson = parseResponse(credentialCreationOptions);
-        logResponse(credentialCreationOptionsJson);
-        credentialCreationOptionsJson.publicKey.challenge = base64ToUint8Array(credentialCreationOptionsJson.publicKey.challenge);
-        credentialCreationOptionsJson.publicKey.user.id = base64ToUint8Array(credentialCreationOptionsJson.publicKey.user.id);
-        credentialCreationOptionsJson.publicKey.excludeCredentials.forEach((credential) => {
-            credential.id = base64ToUint8Array(credential.id)
-        });
-        logNavigatorCreate(credentialCreationOptionsJson);
-        return navigator.credentials.create({publicKey: credentialCreationOptionsJson.publicKey});
+        return navigator.credentials.create(
+            {
+                publicKey: decodeCredentialCreationOptions(credentialCreationOptions).publicKey
+            });
     }).then((publicKeyCredential) => {
         logResponse(publicKeyCredential);
-        const encodedResult = {
-            id: publicKeyCredential.id,
-            rawId: uint8ArrayToBase64(publicKeyCredential.rawId),
-            type: publicKeyCredential.type,
-            response: {
-                attestationObject: uint8ArrayToBase64(publicKeyCredential.response.attestationObject),
-                clientDataJSON: uint8ArrayToBase64(publicKeyCredential.response.clientDataJSON),
-            },
-            clientExtensionResults: publicKeyCredential.getClientExtensionResults()
-        };
         const url = '/register/finish';
         const data = {
-            credential: JSON.stringify(encodedResult),
+            credential: JSON.stringify(encodePublicKeyCredential(publicKeyCredential)),
             username: username,
             tokenName: tokenName
         };
